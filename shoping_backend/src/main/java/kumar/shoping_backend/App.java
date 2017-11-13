5b9a9eceebf79a1a0ba81bb12dbab9eadcad378d
kumarkumarvish <vikash.suman00@gmail.com>
2017-11-13 11:52:27 +0530

    My project setup completed successfully

---
+++ shoping_backend/src/main/java/kumar/shoping_backend/App.java
@@ -0,0 +1,13 @@
+package kumar.shoping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
